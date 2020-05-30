f09e97b27909b5fed7a937f8906557ec8a1f29e4
ben <pingheng.chang@gmail.com>
2020-05-31 01:35:35 +0800

    rm batchNormazie

--- srresnet_edge.py
+++ srresnet_edge.py
@@ -20,10 +20,10 @@ class SRresnetEdge:
 
 	    skip = x
 	    x = tf.nn.conv2d(x, weights['w1'], strides=[1,1,1,1], padding='SAME')
-	    x = tf.layers.batch_normalization(x)
+	    # x = tf.layers.batch_normalization(x)
 	    x = tf.nn.relu(x)
 	    x = tf.nn.conv2d(x, weights['w2'], strides=[1,1,1,1], padding='SAME')
-	    x = tf.layers.batch_normalization(x)
+	    # x = tf.layers.batch_normalization(x)
 	    x = x + skip
 	    return x
 
