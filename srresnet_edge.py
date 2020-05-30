fa9182a65f13d17ea092da86a254a48249010f4e
ben <pingheng.chang@gmail.com>
2020-05-31 00:00:54 +0800

    change optmizer function

--- srresnet_edge.py
+++ srresnet_edge.py
@@ -43,75 +43,75 @@ class SRresnetEdge:
 
 
 	def foward(self, x_in, x_edge, b_block=8):
+		with tf.variable_scope('sr_edge_net') as scope:
+			# y_concate = x
+			print(x_in)
+			x_concate = tf.concat([x_in, x_edge],axis=3, name='x_input_concate')
+			weights ={
+				'w_in': tf.Variable(tf.random_normal([3,3,4,64], stddev=1e-3), name='w_in'),
+				'w_in_residual_out': tf.Variable(tf.random_normal([3,3,64,64], stddev=1e-3), name='w_in_residual_out'),
+				# 'w_in_out': tf.Variable(tf.random_normal([3,3,256,3], stddev=1e-3), name='w_in_residual_out'),
+				# 'w_out': tf.Variable(tf.random_normal([3,3,64,64], stddev=1e-3), name='w_out'),
+				'w_edge': tf.Variable(tf.random_normal([3,3,64,4], stddev=1e-3), name='w_edge'),
+				'w_rect': tf.Variable(tf.random_normal([3,3,68,1], stddev=1e-3), name='w_rect')
+				# 'w_H': tf.Variable(tf.random_normal([3,3,68,1], stddev=1e-3), name='w_H')
 
-		# y_concate = x
-		print(x_in)
-		x_concate = tf.concat([x_in, x_edge],axis=3, name='x_input_concate')
-		weights ={
-			'w_in': tf.Variable(tf.random_normal([3,3,4,64], stddev=1e-3), name='w_in'),
-			'w_in_residual_out': tf.Variable(tf.random_normal([3,3,64,64], stddev=1e-3), name='w_in_residual_out'),
-			# 'w_in_out': tf.Variable(tf.random_normal([3,3,256,3], stddev=1e-3), name='w_in_residual_out'),
-			# 'w_out': tf.Variable(tf.random_normal([3,3,64,64], stddev=1e-3), name='w_out'),
-			'w_edge': tf.Variable(tf.random_normal([3,3,64,4], stddev=1e-3), name='w_edge'),
-			'w_rect': tf.Variable(tf.random_normal([3,3,68,1], stddev=1e-3), name='w_rect')
-			# 'w_H': tf.Variable(tf.random_normal([3,3,68,1], stddev=1e-3), name='w_H')
+			}
 
-		}
+			biases = {
+				'b_in': tf.Variable(tf.zeros([64],name='b_in')),
+				'b_edge': tf.Variable(tf.zeros([4],name='b_edge')),
+				'b_rect': tf.Variable(tf.zeros([1],name='b_rect'))
+			}
 
-		biases = {
-			'b_in': tf.Variable(tf.zeros([64],name='b_in')),
-			'b_edge': tf.Variable(tf.zeros([4],name='b_edge')),
-			'b_rect': tf.Variable(tf.zeros([1],name='b_rect'))
-		}
+			print(x_concate)
+			x = tf.nn.conv2d(x_concate, weights['w_in'], strides=[1,1,1,1], padding='SAME', name='x_input') + biases['b_in']
+			x = tf.nn.relu(x, name='x_input')
+			skip = x
+			for i in range(b_block):
+				x = self.ResidualBlock(x, 3, 64)
 
-		print(x_concate)
-		x = tf.nn.conv2d(x_concate, weights['w_in'], strides=[1,1,1,1], padding='SAME', name='x_input') + biases['b_in']
-		x = tf.nn.relu(x, name='x_input')
-		skip = x
-		for i in range(b_block):
-			x = self.ResidualBlock(x, 3, 64)
+			"""
+			f_output
+			"""
 
-		"""
-		f_output
-		"""
+			x = tf.nn.conv2d(x, weights['w_in_residual_out'], strides=[1,1,1,1], padding='SAME', name='w_in_residual_out')
+			x = tf.layers.batch_normalization(x)
 
-		x = tf.nn.conv2d(x, weights['w_in_residual_out'], strides=[1,1,1,1], padding='SAME', name='w_in_residual_out')
-		x = tf.layers.batch_normalization(x)
+			x_output = tf.math.add(x, skip, name='f_output')
+			# for i in range(1):
+			# 	x = Upsample2xBlock(x, kernel_size=3, filter_size=256)
 
-		x_output = tf.math.add(x, skip, name='f_output')
-		# for i in range(1):
-		# 	x = Upsample2xBlock(x, kernel_size=3, filter_size=256)
+			print(x_output)
 
-		print(x_output)
+			# """
+			# f_edge
+			# """
+			# x_output = tf.nn.conv2d(x_output, weights['w_out'], strides=[1,1,1,1], padding='SAME', name='f_out')
+			# x_output =  tf.nn.relu(x_output, name='x_output')
+			x_edge = tf.nn.conv2d(x_output, weights['w_edge'], strides=[1,1,1,1], padding='SAME', name='f_edge') + biases['b_edge']
+			x_edge = tf.nn.relu(x_edge, name='f_edge')
 
-		# """
-		# f_edge
-		# """
-		# x_output = tf.nn.conv2d(x_output, weights['w_out'], strides=[1,1,1,1], padding='SAME', name='f_out')
-		# x_output =  tf.nn.relu(x_output, name='x_output')
-		x_edge = tf.nn.conv2d(x_output, weights['w_edge'], strides=[1,1,1,1], padding='SAME', name='f_edge') + biases['b_edge']
-		x_edge = tf.nn.relu(x_edge, name='f_edge')
+			# print(x_output)
+			print(x_edge)
+			
+			# """
+			# f_rect
+			# """
+			x_rect = tf.concat([x_output,x_edge],axis=3, name='concate')
+			print(x_rect)
 
-		# print(x_output)
-		print(x_edge)
-		
-		# """
-		# f_rect
-		# """
-		x_rect = tf.concat([x_output,x_edge],axis=3, name='concate')
-		print(x_rect)
+			x_H_hat = tf.nn.conv2d(x_rect, weights['w_rect'], strides=[1,1,1,1], padding='SAME', name='f_rect') + biases['b_rect']
+			x_H_hat = tf.nn.relu(x_H_hat, name='f_rect')
+			print(x_H_hat)
 
-		x_H_hat = tf.nn.conv2d(x_rect, weights['w_rect'], strides=[1,1,1,1], padding='SAME', name='f_rect') + biases['b_rect']
-		x_H_hat = tf.nn.relu(x_H_hat, name='f_rect')
-		print(x_H_hat)
+			# #Here add have some problem
+			x_H = tf.math.add(x_H_hat, x_in, name='x_H_add')
 
-		# #Here add have some problem
-		x_H = tf.math.add(x_H_hat, x_in, name='x_H_add')
-
-		# # print(x_concate)
-		# print(x_H)
-		print(x_H)
-		return x_H, x_edge
+			# # print(x_concate)
+			# print(x_H)
+			print(x_H)
+			return x_H, x_edge
 
 
 	def rect_loss(self, y_HR_hat, y_predict):
@@ -125,5 +125,13 @@ class SRresnetEdge:
 		""" Not sure about joint loss  """
 		return  (rect_loss + 1 * edge_loss)
 
+	# def optimizer(self, loss):
+	# 	return tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
+
 	def optimizer(self, loss):
-		return tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
+		# tf.control_dependencies([discrim_train
+		# update_ops needs to be here for batch normalization to work
+		update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='sr_edge_net')
+		with tf.control_dependencies(update_ops):
+			return tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss, var_list=tf.get_collection(
+				tf.GraphKeys.TRAINABLE_VARIABLES, scope='sr_edge_net'))
