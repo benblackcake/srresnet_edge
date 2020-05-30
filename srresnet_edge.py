65687596601571acd3435b8fcd6f61f3190a2b03
ben <pingheng.chang@gmail.com>
2020-05-31 01:40:19 +0800

    relu after

--- srresnet_edge.py
+++ srresnet_edge.py
@@ -24,6 +24,8 @@ class SRresnetEdge:
 	    x = tf.nn.relu(x)
 	    x = tf.nn.conv2d(x, weights['w2'], strides=[1,1,1,1], padding='SAME')
 	    # x = tf.layers.batch_normalization(x)
+	    x = tf.nn.relu(x)
+	    
 	    x = x + skip
 	    return x
 
