b6d68623ad038099ae46f310a1497c1a7f7d9f89
ben <pingheng.chang@gmail.com>
2020-06-02 00:20:22 +0800

    change total loss func

--- srresnet_edge.py
+++ srresnet_edge.py
@@ -149,8 +149,9 @@ class SRresnetEdge:
 
 	def total_loss(self, y_HR_hat, y_predict_HR, y_edge_HR_hat, y_predict_edge_HR):
 		""" Not sure about joint loss  """
-		return tf.reduce_mean(tf.square(y_HR_hat - y_predict_HR)+
-			self.weight_lamda*(tf.square(tf.math.subtract(y_edge_HR_hat,y_predict_edge_HR))))
+		with tf.variable_scope('sr_edge_net') as scope:
+			return tf.reduce_mean(tf.square(y_HR_hat - y_predict_HR))+\
+			self.weight_lamda*tf.reduce_mean(tf.square(y_edge_HR_hat - y_predict_edge_HR))
 
 	# def optimizer(self, loss):
 	# 	return tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
